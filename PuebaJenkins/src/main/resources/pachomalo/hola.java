81a7d18401182363475f1ec100abfebdb61c25f2
ID <id@correo.com>
2019-10-19 11:15:17 -0500

    hola

---
+++ PuebaJenkins/src/main/resources/pachomalo/hola.java
@@ -0,0 +1,15 @@
+package pachomalo;
+
+public class hola {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helo word"+some(3));
+
+	}
+	
+	public int some(int n) {
+		return n;
+	}
+
+}
